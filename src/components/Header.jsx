2af81c05258a6b7c4577f739fbbf2ecdb751926a
Fernando <nando@Fernandos-MacBook-Air.local>
2026-06-28 18:14:59 +0800

    install zustand, icons, build header

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+"use client"
+import { FaCartArrowDown } from "react-icons/fa"
+
+export default function Header({searchQuery, setSearchQuery}) {
+    return (
+        <div className="flex justify-between bg-gray-950 p-4">
+            <h1 className='text-gray-100 font-extrabold text-4xl'>MarketPlace</h1>
+            <div className="flex gap-2">
+                {/* search input */}
+                <input 
+                    type="search"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    placeholder="Search products..."
+                    className="bg-gray-800 text-gray-100 text-sm rounded-md px-4 py-2 outline-none border border-gray-600 w-40"
+                />
+                {/* cart icon */}
+                <div
+                    className="relative flex items-center cursor-pointer"
+                >
+                    <FaCartArrowDown className="text-2xl" />
+                </div>
+            </div>
+        </div>
+    )
+}
